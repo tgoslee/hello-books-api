06c2d000bda7bcd8ed34e2dc23f36cd6db78afeb
simon <simon@adadevelopersacademy.org>
2021-03-29 10:39:47 -0700

    creates books blueprint, handles creating a book with POST /books

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,17 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import request, Blueprint, Response
 
-hello_world_bp = Blueprint("hello_world_bp", __name__)
-
-
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def hello_world():
-    return "Hello, World!"
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
+@books_bp.route("", methods=["POST"])
+def books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
+    db.session.add(new_book)
+    db.session.commit()
 
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+    return Response(f"Book {new_book.title} successfully created", status=201)
